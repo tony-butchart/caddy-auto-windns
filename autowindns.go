ba840ba44bd4af11cb40a79b26821531b2f206bf
Tony B <87251229+tony-butchart@users.noreply.github.com>
2024-08-02 15:39:41 +1000

    Update autowindns.go

--- autowindns.go
+++ autowindns.go
@@ -55,19 +55,34 @@ func (a *AutoWinDNS) Provision(ctx caddy.Context) error {
 		a.CheckInterval = caddy.Duration(1 * time.Hour)
 	}
 	a.createdRecords = make(map[string]bool)
+
+	a.logger.Info("AutoWinDNS module provisioned",
+		zap.String("server", a.Server),
+		zap.String("zone", a.Zone),
+		zap.Duration("check_interval", time.Duration(a.CheckInterval)))
+
 	return nil
 }
 
 func (a *AutoWinDNS) Start() error {
+	a.logger.Info("AutoWinDNS module starting")
 	go func() {
-		for range a.ctx.Done() {
-			a.updateCNAMERecords()
+		for {
+			select {
+			case <-a.ctx.Done():
+				a.logger.Info("AutoWinDNS context done, stopping updates")
+				return
+			case <-time.After(time.Duration(a.CheckInterval)):
+				a.logger.Info("AutoWinDNS updating CNAME records")
+				a.updateCNAMERecords()
+			}
 		}
 	}()
 	return nil
 }
 
 func (a *AutoWinDNS) Stop() error {
+	a.logger.Info("AutoWinDNS module stopping")
 	return nil
 }
 
@@ -75,12 +90,14 @@ func (a *AutoWinDNS) updateCNAMERecords() {
 	a.mutex.Lock()
 	defer a.mutex.Unlock()
 
+	a.logger.Info("Fetching hostnames from config")
 	hostnames, err := a.getHostnamesFromConfig()
 	if err != nil {
-		a.logger.Error("failed to get hostnames from config", zap.Error(err))
+		a.logger.Error("Failed to get hostnames from config", zap.Error(err))
 		return
 	}
 
+	a.logger.Info("Updating CNAME records", zap.Int("count", len(hostnames)))
 	for _, hostname := range hostnames {
 		alias := strings.Split(hostname, ".")[0]
 		if a.createdRecords[alias] {
@@ -89,14 +106,14 @@ func (a *AutoWinDNS) updateCNAMERecords() {
 		}
 		err := a.createCNAMERecord(alias)
 		if err != nil {
-			a.logger.Error("failed to create CNAME record",
+			a.logger.Error("Failed to create CNAME record",
 				zap.String("hostname", hostname),
 				zap.String("alias", alias),
 				zap.Error(err))
 			continue
 		}
 		a.createdRecords[alias] = true
-		a.logger.Info("successfully created CNAME record",
+		a.logger.Info("Successfully created CNAME record",
 			zap.String("hostname", hostname),
 			zap.String("alias", alias))
 	}
@@ -126,6 +143,8 @@ func (a *AutoWinDNS) getHostnamesFromConfig() ([]string, error) {
 func (a *AutoWinDNS) createCNAMERecord(alias string) error {
 	cmd := fmt.Sprintf("Add-DnsServerResourceRecordCName -Name %s -ZoneName %s -HostNameAlias %s", alias, a.Zone, a.Target)
 
+	a.logger.Info("Creating CNAME record", zap.String("alias", alias), zap.String("cmd", cmd))
+
 	maxRetries := 3
 	for retry := 0; retry < maxRetries; retry++ {
 		err := a.executeSSHCommand(cmd)
@@ -150,12 +169,15 @@ func (a *AutoWinDNS) executeSSHCommand(cmd string) error {
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
+	a.logger.Info("Dialing SSH", zap.String("server", a.Server))
+
 	client, err := ssh.Dial("tcp", a.Server+":22", config)
 	if err != nil {
 		return fmt.Errorf("failed to dial SSH: %w", err)
 	}
 	defer client.Close()
 
+	a.logger.Info("Creating SSH session")
 	session, err := client.NewSession()
 	if err != nil {
 		return fmt.Errorf("failed to create SSH session: %w", err)
@@ -166,9 +188,12 @@ func (a *AutoWinDNS) executeSSHCommand(cmd string) error {
 	session.Stdout = &output
 	session.Stderr = &stderr
 
+	a.logger.Info("Running SSH command", zap.String("cmd", cmd))
 	if err := session.Run(cmd); err != nil {
 		return fmt.Errorf("failed to run command: %w, stderr: %s", err, stderr.String())
 	}
+
+	a.logger.Info("SSH command output", zap.String("output", output.String()))
 
 	return nil
 }
@@ -233,6 +258,13 @@ func (a *AutoWinDNS) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	if a.Target == "" {
 		return d.Err("target is required")
 	}
+
+	a.logger.Info("Unmarshaled AutoWinDNS configuration",
+		zap.String("server", a.Server),
+		zap.String("username", a.Username),
+		zap.String("zone", a.Zone),
+		zap.String("target", a.Target),
+		zap.Duration("check_interval", time.Duration(a.CheckInterval)))
 
 	return nil
 }
